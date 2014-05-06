9b91fe80e2a54520ac4f4a24988dd061f8142d96
Uma Girkar <umag@mit.edu>
2014-05-06 03:02:13 -0400

    Pretty much all tests compatible except for use of the render method.

--- src/pingball/tests/GadgetTests.java
+++ src/pingball/tests/GadgetTests.java
@@ -9,20 +9,21 @@ import physics.Vect;
 import pingball.board.Ball;
 import pingball.board.CircularBumper;
 import pingball.board.SquareBumper;
+import pingball.board.TriangularBumper;
 
 public class GadgetTests {
 
     // Initialize some coordinates on the board that we will put gadgets in
     // later
-    private static Coords coord1;
-    private static Coords coord2;
-    private static Coords coord3;
-    private static Coords coord4;
-    private static Coords coord5;
-    private static Coords coord6;
-    private static Coords coord7;
-    private static Coords coord8;
-    private static Coords coord9;
+    private static Vect coord1;
+    private static Vect coord2;
+    private static Vect coord3;
+    private static Vect coord4;
+    private static Vect coord5;
+    private static Vect coord6;
+    private static Vect coord7;
+    private static Vect coord8;
+    private static Vect coord9;
 
     // Initialize some velocities on the board that we will put gadgets in
     // later
@@ -33,15 +34,15 @@ public class GadgetTests {
 
     @BeforeClass
     public static void setUpBeforeClass() throws Exception {
-        coord1 = new Coords(0, 0);
-        coord2 = new Coords(1, 1);
-        coord3 = new Coords(6, 5);
-        coord4 = new Coords(19, 0);
-        coord5 = new Coords(19, 19);
-        coord6 = new Coords(6, 2);
-        coord7 = new Coords(10, 5);
-        coord8 = new Coords(6, 10);
-        coord9 = new Coords(2, 5);
+        coord1 = new Vect(0, 0);
+        coord2 = new Vect(1, 1);
+        coord3 = new Vect(6, 5);
+        coord4 = new Vect(19, 0);
+        coord5 = new Vect(19, 19);
+        coord6 = new Vect(6, 2);
+        coord7 = new Vect(10, 5);
+        coord8 = new Vect(6, 10);
+        coord9 = new Vect(2, 5);
 
         vel1 = new Vect(1, 0);
         vel2 = new Vect(0, 1);
@@ -57,191 +58,191 @@ public class GadgetTests {
     // CIRCLE BUMPERS
     @Test
     public void circularBumperBounceTest() {
-        Ball b = new Ball(coord9, vel1);
+        Ball b = new Ball("b1", coord9, vel1);
         CircularBumper circBump = new CircularBumper(coord3, "CIRCLE");
-        Ball result = circBump.bounce(b, "circle");
-        assertTrue(result.getVelocity().x() == -1);
-        assertTrue(result.getVelocity().y() == 0);
+        circBump.reactBall(b);
+        assertTrue(b.getVelocity().x() == -1);
+        assertTrue(b.getVelocity().y() == 0);
     }
 
     // SQUARE BUMPERS
     @Test
     public void squareBumperBounceTopTest() {
-        Ball b = new Ball(coord6, vel2);
+        Ball b = new Ball("b2", coord6, vel2);
 
         SquareBumper squareBump = new SquareBumper(coord3, "SQUARE");
-        Ball result = squareBump.bounce(b, "top");
+        squareBump.reactBall(b);
 
-        assertTrue(result.getVelocity().x() == 0);
-        assertTrue(result.getVelocity().y() == -1);
+        assertTrue(b.getVelocity().x() == 0);
+        assertTrue(b.getVelocity().y() == -1);
     }
 
     // NEED TO FIX THIS
     @Test
     public void squareBumperBounceRightTest() {
-        Ball b = new Ball(coord7, vel3);
+        Ball b = new Ball("b3", coord7, vel3);
 
-        Bumper squareBump = new Bumper(coord3, "SQUARE");
-        Ball result = squareBump.bounce(b, "right");
+        SquareBumper squareBump = new SquareBumper(coord3, "SQUARE");
+        squareBump.reactBall(b);
 
-        assertTrue(result.getVelocity().x() == 0);
-        assertTrue(result.getVelocity().y() == -1);
+        assertTrue(b.getVelocity().x() == 0);
+        assertTrue(b.getVelocity().y() == -1);
     }
 
     @Test
     public void squareBumperBounceLeftTest() {
-        Ball b = new Ball(coord9, vel1);
+        Ball b = new Ball("b1", coord9, vel1);
 
-        Bumper squareBump = new Bumper(coord3, "SQUARE");
-        Ball result = squareBump.bounce(b, "left");
+        SquareBumper squareBump = new SquareBumper(coord3, "SQUARE");
+        squareBump.reactBall(b);
 
-        assertTrue(result.getVelocity().x() == -1);
-        assertTrue(result.getVelocity().y() == 0);
+        assertTrue(b.getVelocity().x() == -1);
+        assertTrue(b.getVelocity().y() == 0);
     }
 
     @Test
     public void squareBumperBounceBottomTest() {
-        Ball b = new Ball(coord8, vel4);
+        Ball b = new Ball("b1", coord8, vel4);
 
-        Bumper squareBump = new Bumper(coord3, "SQUARE");
-        Ball result = squareBump.bounce(b, "bottom");
+        SquareBumper squareBump = new SquareBumper(coord3, "SQUARE");
+        squareBump.reactBall(b);
 
-        assertTrue(result.getVelocity().x() == 0);
-        assertTrue(result.getVelocity().y() == 1);
+        assertTrue(b.getVelocity().x() == 0);
+        assertTrue(b.getVelocity().y() == 1);
     }
 
     // TRIANGLE BUMPERS
     @Test
     public void triangle0BumperBounceLegHTest() {
-        Ball b = new Ball(coord6, vel2);
-        Bumper triBump = new Bumper(coord3, "TRIANGLE");
+        Ball b = new Ball("b1", coord6, vel2);
+        TriangularBumper triBump = new TriangularBumper(coord3, "TRIANGLE");
 
-        Ball result = triBump.bounce(b, "hLeg");
+        triBump.reactBall(b);
 
-        assertTrue(result.getVelocity().x() == 0);
-        assertTrue(result.getVelocity().y() == -1);
+        assertTrue(b.getVelocity().x() == 0);
+        assertTrue(b.getVelocity().y() == -1);
     }
 
     @Test
     public void triangle0BumperBounceLegVTest() {
-        Ball b = new Ball(coord9, vel1);
+        Ball b = new Ball("b1", coord9, vel1);
 
-        Bumper triBump = new Bumper(coord3, "TRIANGLE");
-        Ball result = triBump.bounce(b, "vLeg");
+        TriangularBumper triBump = new TriangularBumper(coord3, "TRIANGLE");
+        triBump.reactBall(b);
 
-        assertTrue(result.getVelocity().x() == -1);
-        assertTrue(result.getVelocity().y() == 0);
+        assertTrue(b.getVelocity().x() == -1);
+        assertTrue(b.getVelocity().y() == 0);
     }
 
     @Test
     public void triangle0BumperBounceHypotenuseTest() {
-        Ball b = new Ball(coord7, vel3);
-        Bumper triBump = new Bumper(coord3, "TRIANGLE");
+        Ball b = new Ball("b1", coord7, vel3);
+        TriangularBumper triBump = new TriangularBumper(coord3, "TRIANGLE");
 
-        Ball result = triBump.bounce(b, "hypotenuse");
+        triBump.reactBall(b);
 
-        assertTrue(result.getVelocity().x() == 0);
-        assertTrue(result.getVelocity().y() == 1);
+        assertTrue(b.getVelocity().x() == 0);
+        assertTrue(b.getVelocity().y() == 1);
     }
 
     @Test
     public void triangle90BumperBounceLegHTest() {
-        Ball b = new Ball(coord6, vel2);
-        Bumper triBump = new Bumper(coord3, "TRIANGLE");
+        Ball b = new Ball("b1", coord6, vel2);
+        TriangularBumper triBump = new TriangularBumper(coord3, "TRIANGLE");
 
-        Ball result = triBump.bounce(b, "hLeg");
+        triBump.reactBall(b);
 
-        assertTrue(result.getVelocity().x() == 0);
-        assertTrue(result.getVelocity().y() == -1);
+        assertTrue(b.getVelocity().x() == 0);
+        assertTrue(b.getVelocity().y() == -1);
     }
 
     @Test
     public void triangle90BumperBounceLegVTest() {
-        Ball b = new Ball(coord7, vel3);
-        Bumper triBump = new Bumper(coord3, "TRIANGLE");
+        Ball b = new Ball("b1", coord7, vel3);
+        TriangularBumper triBump = new TriangularBumper(coord3, "TRIANGLE");
 
-        Ball result = triBump.bounce(b, "vLeg");
+        triBump.reactBall(b);
 
-        assertTrue(result.getVelocity().x() == 1);
-        assertTrue(result.getVelocity().y() == 0);
+        assertTrue(b.getVelocity().x() == 1);
+        assertTrue(b.getVelocity().y() == 0);
     }
 
     @Test
     public void triangle90BumperBounceHypotenuseTest() {
-        Ball b = new Ball(coord9, vel1);
+        Ball b = new Ball("b1", coord9, vel1);
 
-        Bumper triBump = new Bumper(coord3, "TRIANGLE");
-        Ball result = triBump.bounce(b, "hypotenuse");
+        TriangularBumper triBump = new TriangularBumper(coord3, "TRIANGLE");
+        triBump.reactBall(b);
 
-        assertTrue(result.getVelocity().x() == 0);
-        assertTrue(result.getVelocity().y() == -1);
+        assertTrue(b.getVelocity().x() == 0);
+        assertTrue(b.getVelocity().y() == -1);
     }
 
     @Test
     public void triangle180BumperBounceLegHTest() {
-        Ball b = new Ball(coord8, vel4);
+        Ball b = new Ball("b1", coord8, vel4);
 
-        Bumper triBump = new Bumper(coord3, "TRIANGLE");
-        Ball result = triBump.bounce(b, "hLeg");
+        TriangularBumper triBump = new TriangularBumper(coord3, "TRIANGLE");
+        triBump.reactBall(b);
 
-        assertTrue(result.getVelocity().x() == 0);
-        assertTrue(result.getVelocity().y() == 1);
+        assertTrue(b.getVelocity().x() == 0);
+        assertTrue(b.getVelocity().y() == 1);
     }
 
     @Test
     public void triangle180BumperBounceLegVTest() {
-        Ball b = new Ball(coord7, vel3);
-        Bumper triBump = new Bumper(coord3, "TRIANGLE");
+        Ball b = new Ball("b1", coord7, vel3);
+        TriangularBumper triBump = new TriangularBumper(coord3, "TRIANGLE");
 
-        Ball result = triBump.bounce(b, "vLeg");
+        triBump.reactBall(b);
 
-        assertTrue(result.getVelocity().x() == 1);
-        assertTrue(result.getVelocity().y() == 0);
+        assertTrue(b.getVelocity().x() == 1);
+        assertTrue(b.getVelocity().y() == 0);
     }
 
     @Test
     public void triangle180BumperBounceHypotenuseTest() {
-        Ball b = new Ball(coord9, vel1);
+        Ball b = new Ball("b1", coord9, vel1);
 
-        Bumper triBump = new Bumper(coord3, "TRIANGLE");
-        Ball result = triBump.bounce(b, "hypotenuse");
+        TriangularBumper triBump = new TriangularBumper(coord3, "TRIANGLE");
+        triBump.reactBall(b);
 
-        assertTrue(result.getVelocity().x() == 0);
-        assertTrue(result.getVelocity().y() == -1);
+        assertTrue(b.getVelocity().x() == 0);
+        assertTrue(b.getVelocity().y() == -1);
     }
 
     @Test
     public void triangle270BumperBounceLegHTest() {
-        Ball b = new Ball(coord8, vel4);
+        Ball b = new Ball("b1", coord8, vel4);
 
-        Bumper triBump = new Bumper(coord3, "TRIANGLE");
-        Ball result = triBump.bounce(b, "hLeg");
+        TriangularBumper triBump = new TriangularBumper(coord3, "TRIANGLE");
+        triBump.reactBall(b);
 
-        assertTrue(result.getVelocity().x() == 0);
-        assertTrue(result.getVelocity().y() == 1);
+        assertTrue(b.getVelocity().x() == 0);
+        assertTrue(b.getVelocity().y() == 1);
     }
 
     @Test
     public void triangle270BumperBounceLegVTest() {
-        Ball b = new Ball(coord9, vel1);
+        Ball b = new Ball("b1", coord9, vel1);
 
-        Bumper triBump = new Bumper(coord3, "TRIANGLE");
-        Ball result = triBump.bounce(b, "vLeg");
+        TriangularBumper triBump = new TriangularBumper(coord3, "TRIANGLE");
+        triBump.reactBall(b);
 
-        assertTrue(result.getVelocity().x() == -1);
-        assertTrue(result.getVelocity().y() == 0);
+        assertTrue(b.getVelocity().x() == -1);
+        assertTrue(b.getVelocity().y() == 0);
 
     }
 
     @Test
     public void triangle270BumperBounceHypotenuseTest() {
-        Ball b = new Ball(coord7, vel3);
-        Bumper triBump = new Bumper(coord3, "TRIANGLE");
+        Ball b = new Ball("b1", coord7, vel3);
+        TriangularBumper triBump = new TriangularBumper(coord3, "TRIANGLE");
 
-        Ball result = triBump.bounce(b, "hypotenuse");
+        triBump.reactBall(b);
 
-        assertTrue(result.getVelocity().x() == 0);
-        assertTrue(result.getVelocity().y() == 1);
+        assertTrue(b.getVelocity().x() == 0);
+        assertTrue(b.getVelocity().y() == 1);
     }
 
 }
