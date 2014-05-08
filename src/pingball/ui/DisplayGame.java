b41faa05cf57c7b89dab43b0469ecc1c8cf15096
Simanta Gautam <simanta@mit.edu>
2014-05-07 23:29:07 -0400

    working on gui design

---
+++ src/pingball/ui/DisplayGame.java
@@ -0,0 +1,18 @@
+package pingball.ui;
+
+import javax.swing.JPanel;
+
+import pingball.board.Board;
+
+public class DisplayGame extends JPanel {
+    
+    //Default JPanel that displays only the menu options 
+    public DisplayGame(){
+        
+    }
+    
+    public DisplayGame(Board b){
+        
+    }
+
+}
