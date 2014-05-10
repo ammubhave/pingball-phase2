4465309bfa7047295c15b1ec64ea54df03cdf758
Amol Bhave <ammubhave@gmail.com>
2014-05-10 09:39:41 -0400

    Remove unused code, fix stuff

--- src/pingball/board/Ball.java
+++ src/pingball/board/Ball.java
@@ -11,62 +11,29 @@ import physics.Vect;
  */
 
 public class Ball {
-
     /**
-     * Thread Safety Information: The ball class will be threadsafe because it
-     * will be immutable. Therefore, since there is no rep exposure and all
-     * fields are final and private, the class is threadsafe.
+     * Thread Safety Information: The ball class will be threadsafe because
+     * all access to position or velocity first synchronize on them separately.
+     * Vect is immutable so makes those parts of code thread safe (prevents rep exposure)
      */
 
     private final static double RADIUS = 0.25;
-    private double mu1 = 0.025;
-    private double mu2 = 0.025;
-    private double gravity = 25;
-    private final Circle circle;
 
     private Vect position;
     private Vect velocity;
 
-    private String name;
-
-    public Ball(String ballName, Vect pos, Vect vel) {
-        this.position = pos;
-        this.circle = new Circle(position, RADIUS);
-        this.velocity = vel;
-        name = ballName;
-    }
+    private final String name;
 
     /**
-     * The environment of the ball is changed from default friction and gravity
-     * settings
-     * 
-     * @param: first friction coefficient
-     * @param: second friction coefficient
-     * @param: gravity constant
+     * Creates a new Ball object
+     * @param ballName the name of the ball
+     * @param pos the position of the ball
+     * @param vel the velocity of the ball
      */
-    public void changeEnvironment(double m1, double m2, double g) {
-        mu1 = m1;
-        mu2 = m2;
-        gravity = g;
-    }
-
-    /** The ball is moved as long as the time is going */
-    public void move(double time) {
-        Vect oldVel = this.getVelocity();
-        // fricvel = oldvel(1 - MU*time - MU2*time*abs(oldvel))
-        Vect fricVel = oldVel.times(1 - (mu1 * time) - (mu2 * time * oldVel.length()));
-
-        // gravVel = fricVel + accel * time, where accel is 0 in x and -25 in y
-        Vect grav = new Vect(0, -gravity);
-        Vect gravVel = fricVel.plus(grav.times(time));
-        // i don't want to find the position vector
-        // deltax = vgrav * deltat
-        Vect deltaX = gravVel.times(time);
-        deltaX = new Vect(deltaX.x(), -deltaX.y());
-
-        this.changeVelocity(gravVel);
-        this.changePos(this.getPos().plus(deltaX));
-        // yay i think it's done!
+    public Ball(String ballName, Vect pos, Vect vel) {
+        this.position = pos;
+        this.velocity = vel;
+        name = ballName;
     }
 
     /**
@@ -92,20 +59,9 @@ public class Ball {
     }
 
     /**
-     * Returns velocity of the ball
-     * 
-     * @return Vect form of velocity
-     */
-    public Vect getFlippedVelocity() {
-        double newX = velocity.x();
-        double newY = velocity.y();
-        return new Vect(newX, -newY);
-    }
-
-    /**
      * Returns the name of the ball
      * 
-     * @return Vect form of velocity
+     * @return name of the ball
      */
     public String getName() {
         return name;
@@ -122,25 +78,19 @@ public class Ball {
     /**
      * Changes position of the ball.
      * 
-     * @param newPos
-     *            new position of the ball
+     * @param newPos new position of the ball
      */
     public void changePos(Vect newPos) {
-        double newX = newPos.x();
-        double newY = newPos.y();
-        this.position = new Vect(newX, newY);
+        this.position = newPos;
     }
 
     /**
      * Changes velocity of the ball.
      * 
-     * @param newVel
-     *            new velocity of the ball
+     * @param newVel new velocity of the ball
      */
     public void changeVelocity(Vect newVel) {
-        double newX = newVel.x();
-        double newY = newVel.y();
-        this.velocity = new Vect(newX, newY);
+        this.velocity = newVel;
     }
     
     /**
