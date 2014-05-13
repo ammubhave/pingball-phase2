e29befbed2c36aff3c3a9ad08cc88949ebb920ed
Amol Bhave <ammubhave@gmail.com>
2014-05-12 21:45:04 -0400

    Fix stopping printing

--- src/pingball/ui/MainWindow.java
+++ src/pingball/ui/MainWindow.java
@@ -3,6 +3,8 @@ package pingball.ui;
 import java.awt.BorderLayout;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.io.File;
 
 import javax.swing.JFrame;
@@ -27,6 +29,12 @@ public class MainWindow extends JFrame implements KeyListener {
         this.add(new DisplayMenu(this, board, host, port, file), BorderLayout.NORTH);
         this.pack();
         this.setVisible(true);
+        
+        this.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                clientControl.stop();
+            }
+        });
     }
 
     public void startController() {
